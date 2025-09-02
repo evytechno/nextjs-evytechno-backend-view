d591e4f53a286c41771b437c1884edf01b017a8e
Priyanshu1121 <priyanshu.chouhan@createbytes.com>
2025-09-02 17:13:23 +0530

    (FIX) Sidebar logo (FIX) Navlinks icons (only available ones)

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,10 +1,13 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import home from "@/public/static/home.png";
+import blog from "@/public/static/blog.png";
+import Image from "next/image";
 
 const links = [
-  { name: "Home", href: "/dashboard", icon: "" },
-  { name: "Blog", href: "/dashboard/blog", icon: "" },
+  { name: "Home", href: "/dashboard", icon: home },
+  { name: "Blog", href: "/dashboard/blog", icon: blog },
   { name: "Service", href: "/dashboard/service", icon: "" },
   { name: "Element", href: "/dashboard/element", icon: "" },
   { name: "Pages", href: "/dashboard/pages", icon: "" },
@@ -20,13 +23,15 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex w-full h-[35px] grow items-center justify-center gap-2 rounded-md text-sm p-3 font-medium text-[#9DA4AE] hover:text-white",
+              "flex w-full h-[40px] py-2 px-5 grow items-center justify-items-center justify-start  gap-4  text-sm  font-medium text-[#9DA4AE] hover:text-white",
               {
-                "bg-white/10 font-semibold text-white": pathname === link.href,
+                "bg-white/10 font-semibold text-white border-r-3 border-r-white/90":
+                  pathname === link.href,
               }
             )}
           >
-            <p>{link.name}</p>
+            <Image src={link.icon} alt={link.name} width={20} height={20} />
+            <p className="">{link.name}</p>
           </Link>
         );
       })}
--- app/ui/dashboard/sidebar.tsx
+++ app/ui/dashboard/sidebar.tsx
@@ -2,16 +2,21 @@
 
 import Link from "next/link";
 import NavLinks from "./nav-links";
+import Image from "next/image";
+
+import logo from "@/public/static/logo.png";
 
 export default function SideBar() {
   return (
-    <div className="relative flex h-screen flex-col gap-3.5 bg-[#1C2536] py-2 px-5 overflow-hidden">
+    <div className="relative flex h-screen min-w-[200px] flex-col gap-3.5 bg-[#1C2536]  overflow-hidden">
       <Link
-        className="text-white text-lg font-semibold justify-start items-end mb-3"
+        className="text-white text-lg font-semibold  flex justify-center items-center max-w-full  "
         href="/dashboard"
       >
-        EvyTechno
+        <Image src={logo} alt="logo" height={40} />
+        <span> EvyTechno</span>
       </Link>
+      <hr className="text-white/75" />
 
       <div className="flex flex-col gap-2.5 items-start ">
         <NavLinks />
