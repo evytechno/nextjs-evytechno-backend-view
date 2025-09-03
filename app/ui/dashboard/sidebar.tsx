5952b046fd9eef0dadda973bc645d273d0c4c944
Priyanshu1121 <priyanshu.chouhan@createbytes.com>
2025-09-03 14:28:04 +0530

    (FIX) Sidebar new logo

--- app/ui/dashboard/sidebar.tsx
+++ app/ui/dashboard/sidebar.tsx
@@ -4,16 +4,16 @@ import Link from "next/link";
 import NavLinks from "./nav-links";
 import Image from "next/image";
 
-import logo from "@/public/static/logo.png";
+import logo from "@/public/static/logo-s.png";
 
 export default function SideBar() {
   return (
     <div className="relative flex h-screen min-w-[200px] flex-col gap-3.5 bg-[#1C2536]  overflow-hidden">
       <Link
-        className="text-white text-lg font-semibold  flex justify-center items-center max-w-full  "
+        className="text-white text-lg font-semibold  flex justify-center items-center max-w-full pt-4 "
         href="/dashboard"
       >
-        <Image src={logo} alt="logo" width={100} />
+        <Image src={logo} alt="logo" width={42} />
         {/* <span> EvyTechno</span> */}
       </Link>
       <hr className="text-white/40" />
