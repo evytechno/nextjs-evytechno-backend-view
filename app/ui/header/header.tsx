0da2cefb24df683a9241f246f4e1538eca6bcf08
Priyanshu1121 <priyanshu.chouhan@createbytes.com>
2025-09-15 12:48:54 +0530

    (FIX) Header fix

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -5,7 +5,7 @@ import HeaderTitle from "../page-header/header-title";
 
 const Header = () => {
   return (
-    <div className="flex pl-4 w-full justify-between items-center pb-4 pt-3 pr-4 sticky top-0 bg-white/20 backdrop-blur-sm shadow">
+    <div className="flex pl-4 w-full justify-between items-center pb-4 pt-3 pr-4 sticky top-0 bg-white/20 backdrop-blur-sm shadow layout-header">
       <Link href="/dashboard">
         <HeaderTitle>Admin Panel</HeaderTitle>
       </Link>
